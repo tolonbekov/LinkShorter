176643033bebb2cd5ac421dfcab33c2d224587e9
dan <tolonbekov@yandex.com>
2018-10-11 02:46:47 +0600

    RestApp.class, LinkResoures.class, commented getAllLinks, add MongoClient add MongoDatabase change getUrlById() change shortUrld add randomId()

--- src/main/java/com/danil/resources/LinkResource.java
+++ src/main/java/com/danil/resources/LinkResource.java
@@ -1,46 +1,89 @@
 package com.danil.resources;
 
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoWriteException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 import javax.imageio.stream.MemoryCacheImageInputStream;
+import javax.print.Doc;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Path("links")
 public class LinkResource {
 
-    private static final AtomicInteger currentId = new AtomicInteger();
+    private static final MongoCollection<Document> LINKS_COLLECTION;
 
-    private static final Map<String, String> links = new ConcurrentHashMap<>();
-
-    private static final StringBuilder result = new StringBuilder();
-
-
+    static {
+        final MongoClient mongo = new MongoClient("localhost", 27017);
+        final MongoDatabase db = mongo.getDatabase("hexlet");
+        LINKS_COLLECTION = db.getCollection("links");
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("{id}")
-    public Response getUrlById(final @PathParam("id") String id){
+    public Response getUrlById(final @PathParam("id") String id) {
         if (id == null || id.isEmpty()) {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
-        final String url = links.get(id);
-        if (url == null) {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
+        final FindIterable<Document> resultsIterable = LINKS_COLLECTION.find(new Document("id", id));
+        final Iterator<Document> resultIterator = resultsIterable.iterator();
+        if (!resultIterator.hasNext()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
+        final String url = resultIterator.next().getString("url");
+        if (url == null || url == "") {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
         return Response.ok(url).build();
+
+
     }
+
 
     @PUT
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
     public Response shortUrl(final String url) {
-        final int id = currentId.getAndIncrement();
-        links.put(String.valueOf(id), url);
-        return Response.ok(id).build();
+        int attempt = 0;
+        while (attempt < 5) {
+            final String id = getRandomId();
+            final Document newShortDoc = new Document("id", id);
+            newShortDoc.put("url", url);
+            try {
+                LINKS_COLLECTION.insertOne(newShortDoc);
+                return Response.ok(id).build();
+            } catch (MongoWriteException e) {
+                System.out.println(e.getMessage());
+            }
+            attempt++;
+        }
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    }
+
+    private static String getRandomId() {
+        String possibleCharacters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+        StringBuilder idBuilder = new StringBuilder();
+        Random rnd = new Random();
+        while (idBuilder.length() < 5) {
+            int index = (int) (rnd.nextFloat() * possibleCharacters.length());
+            idBuilder.append(possibleCharacters.charAt(index));
+        }
+        return idBuilder.toString();
     }
 
 
